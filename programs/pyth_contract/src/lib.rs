c18441f303a11381763290ccfa193329be065b4f
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-09-16 18:38:12 +0530

    deprecated method program

--- programs/pyth_contract/src/lib.rs
+++ programs/pyth_contract/src/lib.rs
@@ -1,33 +1,44 @@
 use anchor_lang::prelude::*;
-use pyth_sdk_solana::load_price_feed_from_account_info;
+use pyth_sdk_solana::{load_price_feed_from_account_info};
 use std::str::FromStr;
 
-declare_id!("42249qzKKgXW7MYno4xXMo9ryqzYo1jSUzFt3D4hgofe");
+// This is your program's public key and it will update
+// automatically when you build the project.
+declare_id!("6S31eyqebbTQP1FNWGymhnZMnu837og3d3GVrTs333PM");
+
+const BTC_USDC_FEED: &str = "HovQMDrbAgAYPCmHVSrezcSmkMtXSSUsLDFANExrZh2J";
+const STALENESS_THRESHOLD: u64 = 60; // in seconds
 
 #[program]
-pub mod pyth_contract {
+mod pyth_contract {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
-        ctx.accounts.new_account.data = data;
-        msg!("Changed data to: {}!", data); // Message will show up in the tx logs
+    pub fn fetch_btc_price(ctx: Context<FetchBitcoinPrice>) -> Result<()> {
+        // 1-Fetch latest price
+        let price_account_info = &ctx.accounts.price_feed;
+        let price_feed = load_price_feed_from_account_info(price_account_info).unwrap();
+        let current_timestamp = Clock::get()?.unix_timestamp;
+        let current_price = price_feed.get_price_no_older_than(current_timestamp, STALENESS_THRESHOLD).unwrap();
+
+        // 2-Format display values
+        let display_price = u64::try_from(current_price.price).unwrap() / 10u64.pow(u32::try_from(-current_price.expo).unwrap());
+        let display_confidence = u64::try_from(current_price.conf).unwrap() / 10u64.pow(u32::try_from(-current_price.expo).unwrap());
+
+        // 3-Log result
+        msg!("BTC/USD price: ({} +- {})", display_price, display_confidence);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    // We must specify the space in order to initialize an account.
-    // First 8 bytes are default account discriminator,
-    // next 8 bytes come from NewAccount.data being type u64.
-    // (u64 = 64 bits unsigned integer = 8 bytes)
-    #[account(init, payer = signer, space = 8 + 8)]
-    pub new_account: Account<'info, NewAccount>,
+pub struct FetchBitcoinPrice<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    #[account(address = Pubkey::from_str(BTC_USDC_FEED).unwrap() @ FeedError::InvalidPriceFeed)]
+    pub price_feed: AccountInfo<'info>,
 }
 
-#[account]
-pub struct NewAccount {
-    data: u64
+#[error_code]
+pub enum FeedError {
+    #[msg("Invalid Price Feed")]
+    InvalidPriceFeed,
 }
